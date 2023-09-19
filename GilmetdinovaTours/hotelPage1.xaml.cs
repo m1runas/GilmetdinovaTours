3a7b51e2f0148f115844013103ae1fd313e60959
Ruslana Gilmetdinova <gilmetdinovaruslana@gamil.com>
2023-09-19 16:59:49 +0500

    Каркас приложения

---
+++ GilmetdinovaTours/hotelPage1.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GilmetdinovaTours
+{
+    /// <summary>
+    /// Логика взаимодействия для hotelPage1.xaml
+    /// </summary>
+    public partial class hotelPage1 : Page
+    {
+        public hotelPage1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            manager.MainFrame.Navigate(new addPage1());
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
